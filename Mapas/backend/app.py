4280efab117f488469420f43427edb668982acb0
Giovana Lopes <gilopes2005@gmail.com>
2026-01-02 17:20:49 -0300

    ajuste docker

--- Mapas/backend/app.py
+++ Mapas/backend/app.py
@@ -222,4 +222,4 @@ def delete_todos_pontos(mapa_id):
     return jsonify({"status": "todos excluidos"})
 
 if __name__ == '__main__':
-    app.run(port=5026, debug=True)
+  app.run(host='0.0.0.0', port=5026, debug=True)
